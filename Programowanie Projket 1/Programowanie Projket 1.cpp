172077eefc731107419cb44553ccce4d965e3d84
JayKey <piromaniak@gmail.com>
2015-10-25 10:55:51 +0100

    [general] added function displaying start screen

--- Programowanie Projket 1/Programowanie Projket 1.cpp
+++ Programowanie Projket 1/Programowanie Projket 1.cpp
@@ -1,11 +1,20 @@
 // Programowanie Projket 1.cpp : Defines the entry point for the console application.
-//
+// Opisaæ tu zadanie
 
 #include "stdafx.h"
+#include <iostream>
+#include <conio.h>
 
+using namespace std;
 
-int main()
-{
+void start_screen();
+
+int main() {
+	start_screen();
+	_getch();
     return 0;
 }
 
+void start_screen() {
+	cout << "Programowanie Projekt 1" << "\n";
+}
